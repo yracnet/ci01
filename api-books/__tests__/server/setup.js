f0ff112e27a8928a86748b8999fe1610a400db68
Willyams Yujra <yracnet@gmail.com>
2024-06-13 23:46:42 -0400

    + Test

--- api-books/__tests__/server/setup.js
+++ api-books/__tests__/server/setup.js
@@ -1,24 +1,26 @@
 import { spawn } from "child_process";
 
-export default function startServer() {
+export default function setup() {
   return new Promise((resolve, reject) => {
-    const root = process.cwd();
-    //console.log("Current working directory:", root);
-    const child = spawn("yarn", ["dev"], {
-      cwd: root,
+    console.log("[START] Starting server...");
+    global.__TEST_SERVER__ = spawn("yarn", ["dev"], {
+      stdio: "pipe",
+      cwd: process.cwd(),
       shell: true,
     });
-    global.__TEST_SERVER__ = child;
-    child.stdout.on("data", (data) => {
-      const text = data.toString();
-      console.log("[Server]:", text);
-      if (text.includes("ready")) {
-        setTimeout(resolve, 500);
+    global.__TEST_SERVER__.on("error", (err) => {
+      console.error("[START] Error starting server:", err);
+      reject(err);
+    });
+    global.__TEST_SERVER__.stdout.on("data", (data) => {
+      const message = data.toString();
+      if (message.includes("VITE")) {
+        console.log("[START] Server started successfully.");
+        resolve();
       }
     });
-
-    child.stderr.on("data", (data) => {
-      reject(data);
+    global.__TEST_SERVER__.stderr.on("data", (data) => {
+      console.error("[SERVER ERROR]", data.toString());
     });
   });
 }
--- api-books/__tests__/server/teardown.js
+++ api-books/__tests__/server/teardown.js
@@ -1,31 +1,20 @@
-// export default function teardown() {
-//   global.__TEST_SERVER__.kill("SIGINT");
-//   console.log("[Server]:", "STOP");
-// }
 export default function stopServer() {
   return new Promise((resolve, reject) => {
     const serverProcess = global.__TEST_SERVER__;
     if (!serverProcess) {
-      console.log("No server process to stop.");
+      console.log("[STOP] No server process to stop.");
       resolve();
-      return;
+    } else {
+      console.log("[STOP] Stopping server...");
+      serverProcess.kill("SIGINT");
+      serverProcess.on("exit", (code, signal) => {
+        resolve();
+        process.exit(0);
+      });
+      serverProcess.on("error", (err) => {
+        console.error("[STOP] Error stopping server:", err);
+        reject(err);
+      });
     }
-
-    console.log("Stopping server...");
-    serverProcess.kill("SIGINT");
-    serverProcess.kill("SIGINT");
-
-    serverProcess.on("exit", (code, signal) => {
-      console.log(
-        `Server process exited with code ${code} and signal ${signal}.`
-      );
-      global.__TEST_SERVER__ = null;
-      resolve();
-    });
-
-    serverProcess.on("error", (err) => {
-      console.error("Error stopping server:", err);
-      reject(err);
-    });
   });
 }
