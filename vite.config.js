ca7884e1d95ed8db0f811b0c41ae0fd53c02dcda
Willyams Yujra <yracnet@gmail.com>
2024-06-19 00:41:37 -0400

    + fix id

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,22 @@ import { defineConfig } from "vite";
 import { pluginAPIRoutes } from "vite-plugin-api-routes";
 import pluginReact from "@vitejs/plugin-react-swc";
 
+const htmlPlugin = () => {
+  return {
+    name: "html-transform",
+    transformIndexHtml(html) {
+      return html.replace("__BUILD_TIME__", new Date().toISOString());
+    },
+  };
+};
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     minify: false,
     outDir: "dist/public",
   },
-  define: {
-    "process.env.BUILD_TIME": JSON.stringify(new Date().toLocaleString()),
-  },
   plugins: [
+    htmlPlugin(),
     pluginReact(),
     pluginAPIRoutes({
       routeBase: "api",
